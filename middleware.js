c3031efc5189d166176cbe13fe529ee43649a3f6
its-shreya13 <shreyamk1303@gmail.com>
2025-04-21 09:19:03 +0530

    Fix: await auth().protect in middleware

--- middleware.js
+++ middleware.js
@@ -1,14 +1,15 @@
-import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-    '/dashboard(.*)',
-    '/forum(.*)'
-])
+  "/dashboard(.*)",
+  "/forum(.*)",
+]);
 
-export default clerkMiddleware((auth,req)=>{
-    if(isProtectedRoute(req)) auth().protect();
-})
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth().protect();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
